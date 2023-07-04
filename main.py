76f8ac77f38a98e5caac1267ad63f302d58eeaa6
Kiran <kiran.bacsa@ethz.ch>
2023-07-04 15:18:25 +0200

    simulation for 1DOF oscillator

--- main.py
+++ main.py
@@ -3,6 +3,46 @@
 def main():
 
     # TODO generate data
+    import numpy as np
+    from scipy.integrate import odeint
+
+    np.random.seed(42)
+
+    def duffing_oscillator(y, t, f, k, c, alpha, m):
+        x, v = y  # Unpack the state variables
+
+        dxdt = v
+        dvdt = (f(t) - c * v - k * x - alpha * x ** 3) / m
+
+        return [dxdt, dvdt]
+
+    # Define the parameters and initial conditions
+    stiffness = 1.0  # Stiffness coefficient
+    damping = 0.1  # Damping coefficient
+    nonlinear_stiffness = 0.5  # Nonlinear coefficient
+    mass = 1.0  # Mass of the oscillator
+    initial_conditions = [0.0, 0.0]  # Initial position and velocity
+
+    # Define the time span
+    t_start = 0.0
+    t_end = 100.0
+    dt = 0.01
+    t_span = np.arange(t_start, t_end, dt)
+
+    external_force = lambda t: np.random.normal() * dt * 0.1
+
+    # Integrate the system using odeint
+    solution = odeint(duffing_oscillator, initial_conditions, t_span,
+                      args=(external_force, stiffness, damping, nonlinear_stiffness, mass)
+                      )
+
+    """
+    import matplotlib.pyplot as plt
+
+    plt.plot(solution)
+    plt.show()
+    """
+
 
     # TODO create dataset object
 
@@ -10,7 +50,7 @@ def main():
 
     # TODO write training loop
 
-    
+
 
     return 0
 

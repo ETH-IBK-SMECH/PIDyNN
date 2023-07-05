4c7361fee47f1aca9d74a965e14dda58cbc74b05
Kiran <kiran.bacsa@ethz.ch>
2023-07-05 09:58:38 +0200

    adapt dimensions to time series

--- models/create_model.py
+++ models/create_model.py
@@ -6,15 +6,18 @@ def create_model(
         input_dim,
         hidden_dim,
         output_dim,
+        seq_len,
 ):
 
     if model_type == 'MLP':
         model = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
+            nn.Flatten(start_dim=1),
+            nn.Linear(input_dim*seq_len, hidden_dim*seq_len),
             nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
+            nn.Linear(hidden_dim*seq_len, hidden_dim*seq_len),
             nn.ReLU(),
-            nn.Linear(hidden_dim, output_dim)
+            nn.Linear(hidden_dim*seq_len, output_dim*seq_len),
+            nn.Unflatten(dim=1, unflattened_size=(seq_len, output_dim)),
         )
     else:
         raise NotImplementedError

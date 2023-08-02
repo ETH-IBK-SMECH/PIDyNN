87f221a88fd5ed17872287700f121edf2081e243
Kiran Bacsa <kbacsa-ethz>
2023-08-02 11:37:35 +0800

    added task switch to change I/O depending on task. Additionaly the progress bar is updated manually

--- main.py
+++ main.py
@@ -9,58 +9,61 @@ from models.create_model import create_model
 from models.pinn_models import ParamClipper
 from plotter.plotter import Plotter
 
+
 def main(config: argparse.Namespace) -> int:
     torch.manual_seed(42)
     device = torch.device('cpu')
 
     # Create dataset
     phys_config = {
-        'n_dof' : config.n_dof,
-        'system-type' : config.system_type
+        'n_dof': config.n_dof,
+        'system-type': config.system_type
     }
     phases = ['train', 'val', 'test']
     full_dataset = create_dataset(phys_config, config.sequence_length)
-    train_size = int(0.75 * len(full_dataset))  # training size in number of batches
-    val_size = int(0.125 * len(full_dataset))  # validation size in number of batches
-    test_size = int(0.125 * len(full_dataset))  # testing size in number of batches
-    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size, test_size])
+    train_size = int(0.8 * len(full_dataset))  # training size in number of batches
+    val_size = int(0.1 * len(full_dataset))  # validation size in number of batches
+    test_size = int(0.1 * len(full_dataset))  # testing size in number of batches
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(full_dataset,
+                                                                             [train_size, val_size, test_size])
     datasets = {
         'train': train_dataset,
         'val': val_dataset,
         'test': test_dataset
     }
     dataloaders = {
-        x: DataLoader(dataset=datasets[x], batch_size=config.batch_size, shuffle=True if x == 'train' else False, num_workers=config.num_workers, pin_memory=True) for x in phases}
+        x: DataLoader(dataset=datasets[x], batch_size=config.batch_size, shuffle=True if x == 'train' else False,
+                      num_workers=config.num_workers, pin_memory=True) for x in phases}
 
     # Create model
-    if config.out_channels != 2*config.n_dof:
+    if config.out_channels != 2 * config.n_dof:
         raise Exception("Number of network outputs does not match state vector of simulated model")
     pinn_config = {
-        'n_dof' : config.n_dof,
-        'phys_system_type' : config.phys_system_type,
-        'system_discovery' : config.system_discovery,
-        'm_' : config.m_,
-        'c_' : config.c_,
-        'k_' : config.k_,
-        'kn_' : config.kn_,
-        'alphas' : full_dataset.alphas,
-        'param_norms' : {
-            'm' : 1.0,
-            'c' : 1.0,
-            'k' : 10.0,
-            'kn' : 10.0
+        'n_dof': config.n_dof,
+        'phys_system_type': config.phys_system_type,
+        'system_discovery': config.system_discovery,
+        'm_': config.m_,
+        'c_': config.c_,
+        'k_': config.k_,
+        'kn_': config.kn_,
+        'alphas': None,
+        'param_norms': {
+            'm': 1.0,
+            'c': 1.0,
+            'k': 10.0,
+            'kn': 10.0
         }
     }
-    model = create_model(config.model_type, config.in_channels, config.latent_features, config.out_channels, config.sequence_length, pinn_config)
+    model = create_model(config.model_type, config.in_channels, config.latent_features, config.out_channels,
+                         config.sequence_length, pinn_config)
+    criterion = nn.MSELoss(reduction='sum')
     optimizer = optim.Adam(model.parameters(), lr=config.learning_rate, weight_decay=config.weight_decay)
-    model.set_switches(config.lambdas)
-    clipper = ParamClipper()
 
-    loss_hist = []
     # Training loop
     epoch = 0
     model = model.to(device)
-    for epoch in tqdm(range(config.num_epochs)):
+    progress_bar = tqdm(total=config.num_epochs)  # progress bar moved outward to preserve while loop structure
+    while epoch < config.num_epochs:
         write_string = ''
         write_string += 'Epoch {}\n'.format(epoch)
         for phase in phases:
@@ -71,41 +74,67 @@ def main(config: argparse.Namespace) -> int:
             else:
                 model.eval()
             for i, sample in enumerate(dataloaders[phase]):
-                # This data parsing is specific to the dummy example and will have to be changed
-                targets = sample[..., :2*config.n_dof].to(device).float().requires_grad_()
-                inputs = sample[..., 2*config.n_dof].to(device).float().requires_grad_()
-                force = sample[..., 2*config.n_dof+1:].to(device).float().requires_grad_()
+                # parse data sample
+                state = sample[..., :2 * config.n_dof].to(device).float()
+                force = sample[..., 2 * config.n_dof + 1:].to(device).float()
+                t_span = sample[:, :, 2 * config.n_dof].to(device).float()
+
+                # unnormalize time component
+                t_span = t_span * (datasets[phase].dataset.maximum[2 * config.n_dof] - datasets[phase].dataset.minimum[2 * config.n_dof]) + datasets[phase].dataset.minimum[2 * config.n_dof]
+
+                # switch according to task
+                match config.task:
+                    case 'regression':
+                        inputs = t_span
+                        targets = state
+                    case 'k_plus_1':
+                        inputs = (state[:, :-1], t_span[0, :-1] - t_span[0, :-1].min())  # ODE only needs relative time, possible work-arounds here: https://github.com/rtqichen/torchdiffeq/issues/122
+                        targets = state[:, 1:]
+                    case _:
+                        raise NotImplementedError
+
                 if phase == 'train':
                     optimizer.zero_grad()
-                loss, losses, _ = model.loss_func(config.lambdas, inputs, targets, inputs, force)
+                predictions = model(inputs)
+                loss = criterion(predictions, targets)
+
                 phase_loss += loss.item()
                 if phase == 'train':
-                    predictions = model(inputs)
                     loss.backward()
                     optimizer.step()
-                    model.apply(clipper)
-                    if config.system_discovery:
-                        write_string += '\tSystem Parameters:\tc - {:.4f}\tk - {:.4f}\tkn - {:.4f}\n'.format(model.c_[0,0].item(),model.k_[0,0].item(),model.kn_[0,0].item())
-                    loss_hist.append([loss_it.item() for loss_it in losses] + [loss.item()])
-                    if config.system_discovery:
-                        print('\tSystem Parameters:\tc - {:.4f}\tk - {:.4f}\tkn - {:.4f}\n'.format(model.c_[0,0].item(),model.k_[0,0].item(),model.kn_[0,0].item()))
-                    loss_hist.append([loss_it.item() for loss_it in losses] + [loss.item()])
-            
+
             write_string += '\tLoss {}\n'.format(phase_loss)
-            print(write_string)
+
+        # Uncomment this line if you want to keep track of the loss (preferably deactivate progress bar before that)
+        #print(write_string)
         epoch += 1
+        progress_bar.update(1)
+
+    progress_bar.close()
 
     # plot results
     model.eval()
 
     # plot last sample of dataset
     sample = datasets['test'][-1]
+
     ground_truth = datasets['test'].dataset.get_original(datasets['test'].indices[-1])
-    inputs = torch.from_numpy(sample[..., 2 * config.n_dof:]).to(device).float().unsqueeze(0)
-    predictions = model(inputs).detach().cpu().squeeze().numpy()
+    inputs = torch.from_numpy(sample[:-1, :2 * config.n_dof]).to(device).float().unsqueeze(0)
+    t_span = torch.from_numpy(sample[:, 2 * config.n_dof]).to(device).float().unsqueeze(0)
+    ground_t_span = torch.from_numpy(ground_truth[:, 2 * config.n_dof]).to(device).float().unsqueeze(0)
+    t_span = t_span * (
+            datasets['test'].dataset.maximum[2 * config.n_dof] - datasets['test'].dataset.minimum[2 * config.n_dof]) + \
+             datasets['test'].dataset.minimum[2 * config.n_dof]
+    ground_t_span = ground_t_span * (
+            datasets['test'].dataset.maximum[2 * config.n_dof] - datasets['test'].dataset.minimum[2 * config.n_dof]) + \
+             datasets['test'].dataset.minimum[2 * config.n_dof]
+    t_span = t_span[0] - t_span[0].min()
+    ground_t_span = ground_t_span[0] - ground_t_span[0].min()
+    input_tensor = [inputs, t_span]
+    predictions = model(input_tensor).detach().cpu().squeeze().numpy()
 
     pinn_plotter = Plotter(config.textwidth, config.fontsize, config.fontname)
-    test_figure = pinn_plotter.plot_predictions(config.n_dof, sample, predictions, ground_truth)
+    test_figure = pinn_plotter.plot_predictions(config.n_dof, sample, predictions, ground_truth, t_span, ground_t_span)
     pinn_plotter.show_figure()
 
     return 0
@@ -120,17 +149,17 @@ if __name__ == '__main__':
     parser.add_argument('--system-type', type=str, default='single_dof_duffing')
 
     # nn-model arguments
-    parser.add_argument('--model-type', type=str, default='sdof_pinn')
-    parser.add_argument('--in-channels', type=int, default=1)
-    parser.add_argument('--latent-features', type=int, default=8)
+    parser.add_argument('--model-type', type=str, default='NeuralODE')
+    parser.add_argument('--in-channels', type=int, default=2)
+    parser.add_argument('--latent-features', type=int, default=32)
     parser.add_argument('--out-channels', type=int, default=2)
 
     # pinn arguments
     parser.add_argument('--phys-system-type', type=str, default='duffing_sdof')
     parser.add_argument('--lambdas', type=dict, default={
-        'obs' : 5.0,
-        'cc' : 10.0,
-        'ode' : 5.0
+        'obs': 5.0,
+        'cc': 10.0,
+        'ode': 5.0
     })
     parser.add_argument('--system-discovery', type=bool, default=True)
     parser.add_argument('--m-', type=torch.Tensor, default=torch.Tensor([[10.0]]))
@@ -139,10 +168,11 @@ if __name__ == '__main__':
     parser.add_argument('--kn-', type=torch.Tensor, default=torch.Tensor([[100.0]]))
 
     # training arguments
-    parser.add_argument('--batch-size', type=int, default=128)
+    parser.add_argument('--task', type=str, default="k_plus_1")
+    parser.add_argument('--batch-size', type=int, default=256)
     parser.add_argument('--num-workers', type=int, default=0)
-    parser.add_argument('--num-epochs', type=int, default=100000)
-    parser.add_argument('--sequence-length', type=int, default=8)
+    parser.add_argument('--num-epochs', type=int, default=150)
+    parser.add_argument('--sequence-length', type=int, default=500)
     parser.add_argument('--learning-rate', type=float, default=2e-3)
     parser.add_argument('--weight-decay', type=float, default=1e-4)
 
